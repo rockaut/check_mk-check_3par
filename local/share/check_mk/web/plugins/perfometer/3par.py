ca1d67ae12a0ad9f7e1586f19718d61941b56bf6
Markus Fischbacher <Markus.Fischbacher@infotech.at>
2016-07-06 17:29:28 +0200

    - implemented perfometers ( cpgs, systemfailedcap, systemtotalcap )

---
+++ local/share/check_mk/web/plugins/perfometer/3par.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+# -*- encoding: utf-8; py-indent-offset: 4 -*-
+
+perfometers["check_mk-3par.cpgs"] = perfometer_check_mk_df
+perfometers["check_mk-3par.SystemTotalCapacity"] = perfometer_check_mk_df
+perfometers["check_mk-3par.SystemFailedCapacity"] = perfometer_check_mk_df
